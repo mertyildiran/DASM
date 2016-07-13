5b65378db910602bb4df6cf060948fd3f845bc9e
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-07-13 03:11:07 +0300

    An example assembly program added.

---
+++ examples/counter.asm
@@ -0,0 +1,17 @@
+.data
+     count: 60
+     array: .space 10
+     char: 0xfe
+.code
+       	ldi 0 count
+       	ld  0 0
+       	ldi 1 array
+       	ldi 2 char
+       	ld  2 2
+lpp  	st 1 2
+       	inc 1
+       	dec 0
+       	jz loop
+       	jmp lpp
+loop 	sub 1 2 3
+lp1  	jmp lp1  
